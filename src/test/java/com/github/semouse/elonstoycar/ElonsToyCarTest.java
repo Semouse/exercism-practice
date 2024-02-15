44cb4d20578359910da95114862c73da6226ff90
Leanid Barok <tratos149@gmail.com>
2024-02-15 13:48:58 +0300

    Add solution to Elon's Toy Car.

---
+++ src/test/java/com/github/semouse/elonstoycar/ElonsToyCarTest.java
@@ -0,0 +1,43 @@
+package com.github.semouse.elonstoycar;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+class ElonsToyCarTest {
+
+    @Test
+    void distanceDisplay() {
+        ElonsToyCar car = ElonsToyCar.buy();
+        assertEquals("Driven 0 meters", car.distanceDisplay());
+    }
+
+    @Test
+    void batteryDisplay() {
+        ElonsToyCar car = ElonsToyCar.buy();
+        assertEquals("Battery at 100%", car.batteryDisplay());
+    }
+
+    @Test
+    void drive() {
+        ElonsToyCar car = ElonsToyCar.buy();
+        car.drive();
+        String distance = car.distanceDisplay();
+        String battery = car.batteryDisplay();
+        assertEquals("Battery at 99%", battery);
+        assertEquals("Driven 20 meters", distance);
+    }
+
+    @Test
+    void notDriveIfBatteryEmpty() {
+        ElonsToyCar car = ElonsToyCar.buy();
+        for(int i = 0; i < 101; i++){
+            drive();
+        }
+
+        String distance = car.distanceDisplay();
+        String battery = car.batteryDisplay();
+        assertEquals("Battery empty", battery);
+        assertEquals("Driven 2000 meters", distance);
+    }
+}
