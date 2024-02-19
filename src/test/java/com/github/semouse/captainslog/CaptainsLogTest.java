05624f7e70dea47fc71e42e08d173653cd2392b0
Leanid Barok <tratos149@gmail.com>
2024-02-19 19:18:47 +0300

    Add solution for Captain's Log task.

---
+++ src/test/java/com/github/semouse/captainslog/CaptainsLogTest.java
@@ -0,0 +1,35 @@
+package com.github.semouse.captainslog;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.Random;
+import java.util.stream.IntStream;
+import org.junit.jupiter.api.Test;
+
+class CaptainsLogTest {
+
+    private final CaptainsLog captainsLog = new CaptainsLog(new Random());
+
+    @Test
+    void randomPlanetClass() {
+        char planet = captainsLog.randomPlanetClass();
+        assertTrue(contains(planet));
+    }
+
+    private boolean contains(char target) {
+        return IntStream.range(0, CaptainsLog.PLANET_CLASSES.length)
+            .anyMatch(i -> target == CaptainsLog.PLANET_CLASSES[i]);
+    }
+
+    @Test
+    void randomShipRegistryNumber() {
+        String number = captainsLog.randomShipRegistryNumber();
+        assertEquals(8, number.length());
+    }
+
+    @Test
+    void randomStardate() {
+        double date = captainsLog.randomStardate();
+        assertTrue(date >= 41000.0 && date < 42000);
+    }
+}
