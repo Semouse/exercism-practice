8b07e14c6d7057a75d46a89426e209c1ac7782db
Leanid Barok <tratos149@gmail.com>
2024-02-20 18:49:57 +0300

    Add solution for secrets task.

---
+++ src/test/java/com/github/semouse/SecretsTest.java
@@ -0,0 +1,28 @@
+package com.github.semouse;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+class SecretsTest {
+
+    @Test
+    void shiftBack() {
+        assertEquals(2, Secrets.shiftBack(8, 2));
+    }
+
+    @Test
+    void setBits() {
+        assertEquals(7, Secrets.setBits(5, 3));
+    }
+
+    @Test
+    void flipBits() {
+        assertEquals(14, Secrets.flipBits(5, 11));
+    }
+
+    @Test
+    void clearBits() {
+        assertEquals(4, Secrets.clearBits(5, 11));
+    }
+}
