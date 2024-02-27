b7289d3c49390e4ca12ff2a8d2ab9fb9771ef23e
Leanid Barok <tratos149@gmail.com>
2024-02-27 17:00:13 +0300

    Remove unused imports;

--- src/test/java/com/github/semouse/exercises/microblog/MicroBlogTest.java
+++ src/test/java/com/github/semouse/exercises/microblog/MicroBlogTest.java
@@ -1,9 +1,8 @@
 package com.github.semouse.exercises.microblog;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import java.util.stream.Stream;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
