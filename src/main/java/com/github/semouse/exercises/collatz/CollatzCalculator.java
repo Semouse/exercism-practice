57b36c9592d77cbfed0c68e9cff5bcaaae301757
Leanid Barok <tratos149@gmail.com>
2024-05-11 16:27:54 +0300

    Add solution for 'Collatz' task;

---
+++ src/main/java/com/github/semouse/exercises/collatz/CollatzCalculator.java
@@ -0,0 +1,18 @@
+package com.github.semouse.exercises.collatz;
+
+public class CollatzCalculator {
+
+    int computeStepCount(int start) {
+        if (start <= 0) {
+            throw new IllegalArgumentException("Only positive integers are allowed");
+        }
+
+        int count = 0;
+        while (start != 1) {
+            start = (start % 2 == 0) ? start / 2 : start * 3 + 1;
+            count++;
+        }
+
+        return count;
+    }
+}
