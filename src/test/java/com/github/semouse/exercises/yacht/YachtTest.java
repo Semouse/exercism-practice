369b5df7655372633e8f8379235890f7073a1bc8
Leanid Barok <tratos149@gmail.com>
2024-05-17 17:13:19 +0300

    Add solution for 'Yacht' task;

--- src/test/java/com/github/semouse/exercises/yacht/YachtTest.java
+++ src/test/java/com/github/semouse/exercises/yacht/YachtTest.java
@@ -1,6 +1,5 @@
 package com.github.semouse.exercises.yacht;
 
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
