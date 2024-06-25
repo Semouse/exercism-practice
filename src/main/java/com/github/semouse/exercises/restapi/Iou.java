b862dd5df8fa2516f78c285d8aa9e2d40e962522
Leanid Barok <tratos149@gmail.com>
2024-06-25 14:54:05 +0300

    RestApi task initial commit

---
+++ src/main/java/com/github/semouse/exercises/restapi/Iou.java
@@ -0,0 +1,11 @@
+package com.github.semouse.exercises.restapi;
+
+public class Iou {
+    public final String name;
+    public final double amount;
+
+    public Iou(String name, double amount) {
+        this.name = name;
+        this.amount = amount;
+    }
+}
---
+++ src/main/java/com/github/semouse/exercises/restapi/RestApi.java
@@ -0,0 +1,22 @@
+package com.github.semouse.exercises.restapi;
+
+import org.json.JSONObject;
+
+public class RestApi {
+    RestApi(User... users) {
+        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+    }
+
+    String get(String url) {
+        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+    }
+
+    String get(String url, JSONObject payload) {
+        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+    }
+
+    String post(String url, JSONObject payload) {
+        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+    }
+
+}
